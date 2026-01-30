2c8bf2f7b6f62935e0365065ba2c613d7b4687e0
Annie Tadvick <annie.tadvick@gmail.com>
2026-01-30 17:33:15 -0500

    Part of mil-137: EP0-T5a Lock consumption strategy

--- stencil/stencil.config.ts
+++ stencil/stencil.config.ts
@@ -1,5 +1,56 @@
 /* ================================
-  TL;DR  -->  will contain stencil config for our specific project. 
+  TL;DR  -->  TLDR - Creating custom elements w/react wrappers 
 
-      - need to look up on chatgpt/in the documentation and copy pasta
+    - Custom elements = reusable product artifact
+    - React wrappers = MVP DX layer
+    - Stencil components own all rendering & behavior 
 ================================ */
+
+/* 
+
+! CHOICE MADE - ADD TO READ ME: BOTH
+We treat Stencil custom elements as the canonical UI layer and ship 
+them framework-agnostically. For our React app, we optionally wrap 
+those elements with thin React adapters to improve developer experience 
+while keeping all rendering and logic inside the Web Components.
+*/
+
+import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+
+export const config: Config = {
+    namespace: 'trust-center',
+
+    //Enable shadow DOM to encapsulate style - safe for reuse inside client systems
+    extras: {
+        shadowDomShim: false,
+    }, 
+
+    outputTargets: [ 
+         /* 
+         Custom Elements Output
+          - Framework-agnostic web components.
+          - This is the artifact the client can reuse directly.
+        */
+        {
+            type: 'dist',
+            esmLoaderPath: '../loader',
+        },
+
+        /* 
+        React Wrapper Output
+        - thin adapters for MVP dev experience - basically a translation layer
+        - No logic/state/styling 
+        */
+       reactOutputTarget({
+        componentCorePackage: '@trustcenter/components', 
+        //this is the file path and file that will be auto-generated
+        proxiesFile: '../client/src/stencil/components.ts'
+       }), 
+
+       //Loader Ouput type ensure custom elements are registered once + used by react/any future consumers. 
+       {
+        type: 'dist-custom-elements',
+       },
+    ]
+}
